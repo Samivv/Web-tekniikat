d8b402a108fd48c974c8dd483164e566fe528e2f
Samivv <101732172+Samivv@users.noreply.github.com>
2022-05-18 22:39:33 +0300

    Lisätty kommentteja, sekä eventtejä

--- Tehtävä 3/js.js
+++ Tehtävä 3/js.js
@@ -2,55 +2,56 @@ let tulostaTahan = document.querySelector("[class=generoitusisalto]");
 let uusiElementti = document.createElement("h2")
 let listener1 = document.getElementById("nappi1");
 let listener2 = document.getElementById("nappi2");
-let listener3 = document.getElementById("nappi3");
 let listener4 = document.getElementById("nappi4");
 let listener5 = document.getElementById("varipyora");
+let listener6 = document.getElementById("range");
 let h1 = document.querySelector("h1");
 listener1.addEventListener("click", funktio1);
 listener2.addEventListener("click", funktio2);
-listener3.addEventListener("click", funktio3);
-listener4.addEventListener("click", funktio4);
-
-
+listener6.addEventListener("change", funktio9);
 h1.addEventListener("mouseover", funktio5);
 h1.addEventListener("mouseout", funktio6);
 listener5.addEventListener("change", funktio7);
 document.querySelector("body").addEventListener("keypress", funktio8);
 
+// Lisätään elementti ja lisätään siihen tekstiä, sekä poistetaan poisto-napin esto.
 function funktio1() {
     tulostaTahan.appendChild(uusiElementti);
     document.querySelector("h2").innerText = ("Tähän on nyt lisätty h2 elementti.")
+    listener2.toggleAttribute("disabled");
 }
-
+// Poistetaan lisätty elementti ja estetään napin käyttö turhaan.
 function funktio2() {
     tulostaTahan.removeChild(uusiElementti);
+    listener2.toggleAttribute("disabled");
+}
+// Sivun asettelun muuttaminen
+function funktio9() {
+    if(listener6.value == 2) {
+        document.body.style.justifyContent = "center";
+    } else if (listener6.value == 3) {
+        document.body.style.justifyContent = "right";
+    } else if (listener6.value == 1) {
+        document.body.style.justifyContent = "left";
+    }
 }
 
-function funktio3() {
-    tulostaTahan.removeChild(uusiElementti);
-}
 
-function funktio4() {
-    tulostaTahan.removeChild(uusiElementti);
-}
-
+// Väripyörällä valitaan sivun taustaväri
 function funktio7() {
     document.body.style.background = listener5.value;
 }
-
+// Kertoo jos olet painanut jotain näppäintä.
 function funktio8(event) {
     let y = event.key;
     document.querySelector("span").innerText = " Olet painanut tätä nappia: '" + y+"'";
 }
 
-
-
-
-
+// Kun hiiri osuu kohdille tee tämä 
 function funktio5() {
     h1.style.fontSize = "50px";
 }
-
+// Kun hiiri lähtee pois tee tämä
 function funktio6() {
     h1.style.fontSize = "";
 }
