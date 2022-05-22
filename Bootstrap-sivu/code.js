b13a941f2c3ab99d82e82b285b2968f3dfa39a3f
Samivv <101732172+Samivv@users.noreply.github.com>
2022-05-23 00:26:41 +0300

    poistettu turha koodi

--- Bootstrap-sivu/code.js
+++
@@ -1,8 +0,0 @@
-let y;
-function haeteksti() {
-fetch("https://en.wikipedia.org/w/api.php?action=query&origin=*&prop=extracts&format=json&exintro=&titles=Berlin")
-.then((response) => response.json())
-.then(data => y = data);
-}
-
-haeteksti();
